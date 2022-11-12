7c96511a6cc23f7c280f854746f31323e4eaeabc
Nihal Neeli <95561241+neelinihal290@users.noreply.github.com>
2022-11-12 11:13:23 +0530

    Delete main.java

--- Colllections/src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-      Theatre theatre = new Theatre("mani",8,8);
-      theatre.getTheatreName();
-      theatre.reserveSeats("A13");
-
-    }
-}
